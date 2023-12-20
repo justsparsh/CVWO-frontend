be3098a30737731fe51f6f1d90062c7be7feed07
justsparsh <e1121161@u.nus.edu>
2023-12-20 17:21:24 +0800

    nil

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -5,20 +5,34 @@ import React, {useState} from "react";
 
 const Login: React.FC = () => {
     const [inputValue, setInputValue] = useState<string>('');
+    const [showSignupButton, setShowSignupButton] = useState<boolean>(false);
 
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setInputValue(event.target.value);
       };
 
-      const handleButtonClick = () => {
+    const handleLoginButtonClick = () => {
         // Now you can use the inputValue as needed, e.g., make an API call
         console.log('Button clicked! Input value:', inputValue)};
+        setShowSignupButton(true);
 
+    // const handleSignupButtonClick = () => {
+
+    // };
 
     return (
         <div>
-            <UsernameEntry value={inputValue} onChange={handleInputChange}/>
-            <LoginButton onClick={handleButtonClick}/>
+            <div style={{padding: "25px"}}>
+                <UsernameEntry value={inputValue} onChange={handleInputChange}/>
+            </div>
+
+            <div>
+                <LoginButton onClick={handleLoginButtonClick}/>
+
+                {showSignupButton && (
+                    <LoginButton onClick={handleLoginButtonClick}/>
+                )}
+            </div>
         </div>
     )
 };
